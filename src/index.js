55e490e298a3258b682863c633be7f2b9876a679
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-04-03 23:01:35 +0900

    Added two freaking diagrams (#2)
    * DIP-vis
    
    * bug
    
    * examples
    
    * options
    
    * a
    
    * wip
    
    * fa
    
    * sdaf
    
    * images
    
    * fuck you svelte
    
    * a
    
    * fasf
    
    * af
    
    * a
    
    * a
    
    * a
    
    * images
    
    * unused animation
    
    * bees
    
    * a

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import VisualTOC                      from './diagrams/VisualTOC.html';
+import MyPaintVis                     from './diagrams/MyPaintVis.html';
+import DIPExamples                    from "./diagrams/DIPExamples.html";
+
+import ColabLink                      from './components/ColabLink.html';
 
 // eagerly initialize vtoc  as it's above the fold
+import { sections } from "./sections.json";
 const tocNav = document.getElementById('vtoc');
-const visualTOC = new VisualTOC({target: tocNav});
+const visualTOC = new VisualTOC({target: tocNav, data: {sections: sections}});
 
 // lazily initialize any diagram below the fold. E.G:
 // {
@@ -11,3 +16,30 @@ const visualTOC = new VisualTOC({target: tocNav});
 //     const styleTransferExamples = new StyleTransferExamples({target: figure});
 //   });
 // }
+
+{
+  const figure = document.getElementById("MyPaint-Vis");
+  figure.addEventListener("ready", function() {
+    const myPaintVis = new MyPaintVis({target: figure});
+  });
+}
+
+{
+  const figure = document.getElementById("DIP-Examples");
+  figure.addEventListener("ready", function() {
+    const cppnAnimations = new DIPExamples({target: figure});
+  });
+}
+
+function addColabLinks(sections) {
+  for (const section of sections) {
+    const className = "add-colab-link--" + section.anchor.slice(1);
+    const elements = document.getElementsByClassName(className);
+    for (const element of elements) {
+      const data = {target: element, data: { url: section.colab_url }};
+      const colabLink = new ColabLink(data);
+    }
+  }
+}
+
+addColabLinks(sections);
